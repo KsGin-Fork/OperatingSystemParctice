efb81fc6d129ea44992e47fd71484706cd5e6406
杨帆 <imqqyangfan@gmail.com>
2017-09-26 19:42:00 +0800

    init project

---
+++ ProcessExc/cmake-build-debug/CMakeFiles/ProcessExc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProcessExc.dir/main.cpp.o"
+  "ProcessExc.pdb"
+  "ProcessExc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProcessExc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ProcessExc/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <zconf.h>
+
+int fork1();
+int fork2();
+int fork3();
+
+
+
+
+int fork1(){
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        printf("fork error\n");
+    }
+
+    if (pid == 0) {
+        printf("child process , child id = %d , parent id = %d\n" , getpid() , getppid());
+    }
+
+    if (pid > 0){
+        printf("parent process , child id = %d , parent id = %d\n" , pid , getpid());
+    }
+
+    return 0;
+}
+
+
+int fork2(){
+    pid_t pid;
+    char * msg;
+    int k;
+    printf("process creating study!\n");
+    pid = fork();
+    switch (pid){
+        case 0:
+            msg = const_cast<char *>("Child process is running");
+            k = 3;
+            break;
+        case -1:
+            msg = const_cast<char *>("error");
+            k = 0;
+            break;
+        default:
+            msg = const_cast<char *>("parent process is running");
+            k = 5;
+            break;
+    }
+
+    while (k > 0){
+        puts(msg);
+        k--;
+        sleep(1);
+    }
+    return 0;
+}
+
+int fork3(){
+    pid_t pid;
+    printf("process creating study!\n");
+    pid = fork();
+    switch (pid){
+        case 0:
+           while (true){
+               printf("background process , id = %d , parent id = %d" , getpid() , getppid());
+               sleep(3);
+           }
+        case -1:
+            printf("Error");
+            break;
+        default:
+            printf("parent process , pid = %d\n" + getpid());
+            sleep(3);
+            break;
+    }
+
+    return 0;
+}
+
+int main() {
+    //fork1();
+    //fork2();
+    fork3();
+    return 0;
+}
