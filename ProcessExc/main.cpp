922aa3d9d97f6b7c7ff083387feaae3bc9221b8d
KsGin <imqqyangfan@gmail.com>
2017-10-11 10:04:52 +0800

    add file

--- ProcessExc/main.cpp
+++ ProcessExc/main.cpp
@@ -1,12 +1,10 @@
 #include "ForkTest.h"
-
 #include "ProcessControl.h"
+
 int main(int argc , char* argv[]) {
-    //fork1();
-    //fork2();
-    //fork3();
-
-    ProcessControlTest(argc , argv);
-
+//    fork1();
+//    fork2();
+//    fork3();
+//    ProcessControlTest(argc , argv);
     return 0;
 }
